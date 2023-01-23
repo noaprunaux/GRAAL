f55b5844f2dbb7d5d46cba7ab5b5ff161727714c
Noa Prunaux <noaprunaux@gmail.com>
2023-01-23 11:07:59 +0100

    generate gift ideas API endpoint

---
+++ pages/api/generate-gifts.js
@@ -0,0 +1,25 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) { 
+    const {priceMin, priceMax, gender, age, hobbies} = req.body;
+    const prompt = generatePrompt(priceMin, priceMax, gender, age, hobbies);
+    
+    console.log(prompt);
+
+    const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: prompt,
+        temperature: 0.6,
+      });
+      res.status(200).json({ result: completion.data.choices[0].text });
+
+}
+
+function generatePrompt(priceMin, PriceMax,gender, age, hobbies) {                      // Génére notre question
+  return `suggest 3 Christmas gifts ideas between ${priceMin}euros and ${PriceMax}euros  for a ${age} years old ${gender} that is into ${hobbies}.`
+}
